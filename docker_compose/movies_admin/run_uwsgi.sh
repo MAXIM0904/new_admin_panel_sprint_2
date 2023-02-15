b12322643672bc4350d54213bb821f656f57bdcd
Max <dvdvdvd90@mail.ru>
2023-02-15 16:08:22 +0300

    Commit 4

--- docker_compose/movies_admin/run_uwsgi.sh
+++ docker_compose/movies_admin/run_uwsgi.sh
@@ -7,6 +7,6 @@ chown www-data:www-data /var/log
 python manage.py migrate
 python manage.py compilemessages -l en -l ru
 python manage.py collectstatic --no-input
-#python manage.py createsuperuser --username=UWSGI_ADMINDJ --email=UWSGI_EMAILDJ --password=UWSGI_PASSWORDDJ --noinput || true
+python manage.py createsuperuser --username=$(UWSGI_ADMINDJ) --email=$(UWSGI_EMAILDJ) --password=$(UWSGI_PASSWORDDJ) --noinput || true
 
 uwsgi --strict --ini ./movies_admin/uwsgi.ini
