c7d5e6546d793c1e644aa3b194bd4fd6b37c881f
Max <dvdvdvd90@mail.ru>
2023-02-15 15:02:17 +0300

    Commit 3

--- docker_compose/movies_admin/config/components/database.py
+++ docker_compose/movies_admin/config/components/database.py
@@ -5,7 +5,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'movies_database'),
-        'USER': os.getenv('POSTGRES_USER', 'app'),
+        'USER': os.getenv('POSTGRES_USER', 'movies_admin'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
         'PORT': os.getenv('POSTGRES_PORT', 5432),
